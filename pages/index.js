41ed1e62bd8ad35a0f268d1723d33fd33ad807cc
EmmaLeu <manuela.leu@uipath.com>
2020-09-16 16:07:54 +0300

    Add basic game board component

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function App() {
 
   return (
     <div>
-      <div className = "container">
+      <div className="container">
         <h1>Memory Game</h1>
         <div>High Score: {highScore}</div>
         <div>
@@ -35,10 +35,102 @@ export default function App() {
               </button>
               <button onClick={() => setOptions(null)}>Main Menu</button>
             </>
-          )
-        }
+          )}
         </div>
       </div>
+
+      {
+        options ? (
+          <MemoryGame
+            options={options}
+            setOptions={setOptions}
+            highScore={highScore}
+            setHighScore={setHighScore}/>
+        ) : (
+          <h2>Choose a difficulty to begin!</h2>
+        )}
+
     </div>
   )
+}
+
+function MemoryGame({options, setOptions, highScore, setHighScore}){
+  const [game, setGame] = useState([])
+  const [flippedCount, setFlippedCount] = useState(0)
+  const [flippedIndexes, setFlippedIndexes] = useState([])
+
+  const colors = [
+    '#ecdb54',
+    '#e34132',
+    '#6ca0dc',
+    '#944743',
+    '#dbb2d1',
+    '#ec9787',
+    '#00a68c',
+    '#645394',
+    '#6c4f3d',
+    '#ebe1df',
+    '#bc6ca7',
+    '#bfd833'
+  ]
+
+  useEffect(() => {
+    const newGame = []
+    for(let index = 0; index < options / 2; index++){
+      const firstOption = {
+        id: 2 * index,
+        colorId: index,
+        color: colors[index],
+        flipped: false
+      }
+      const secondOption = {
+        id: 2 * index + 1,
+        colorId: index,
+        color: colors[index],
+        flipped: false
+      }
+
+      newGame.push(firstOption)
+      newGame.push(secondOption)
+    }
+
+    const shuffledGame = newGame.sort(() => Math.random() - 0.5)
+    setGame(shuffledGame)
+  }, [])
+
+  useEffect(() => {
+    //Loads when the game variable changes
+  }, [game])
+
+  if(game.length === 2) {
+    //Runs if 2 cards have been flipped
+  }
+
+  if(game.length === 0) {
+    return <div>Loading...</div>
+  }
+  else {
+    return (
+      <div id="cards">
+        {
+          game.map((card, index) => (
+            <div className="card" key={index}>
+              <Card
+                id={index}
+                color={card.color}
+                game={game}
+                flippedCount={flippedCount}
+                setFlippedCount={setFlippedCount}
+                flippedIndexes={setFlippedIndexes}
+                setFlippedIndexes={setFlippedIndexes}
+              />
+            </div>
+          ))}
+      </div>
+    )
+  }
+}
+
+function Card(props) {
+  return <div>I'm a card</div>
 }
