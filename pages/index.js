9c4ddbe0cac4d118fd8744b786739453f514c9a9
EmmaLeu <manuela.leu@uipath.com>
2020-09-16 15:47:16 +0300

    Add App parent component for the main menu

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,44 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, {useState, useEffect} from "react";
+import {useSpring, animated as a} from "react-spring";
 
-export default function Home() {
+export default function App() {
+  const [options, setOptions] = useState(null)
+  const [highScore, setHighScore] = useState(0)
+
+  useEffect(() =>{
+    //Load when game starts
+  }, [])
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div>
+      <div className = "container">
+        <h1>Memory Game</h1>
+        <div>High Score: {highScore}</div>
+        <div>
+          { options === null? (
+            <>
+              <button onClick={() => {setOptions(12)}}>Easy</button>
+              <button onClick={() => {setOptions(18)}}>Medium</button>
+              <button onClick={() => {setOptions(24)}}>Hard</button>
+            </>
+          ) : (
+            <>
+              <button onClick={() => {
+                  const previousOptions = options
+                  setOptions(null)
+                  setTimeout(() => {
+                    setOptions(previousOptions)
+                  }, 5)
+                }}
+              >
+              Start Over
+              </button>
+              <button onClick={() => setOptions(null)}>Main Menu</button>
+            </>
+          )
+        }
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </div>
     </div>
   )
 }
